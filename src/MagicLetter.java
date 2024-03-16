27df15941fdcbb78e4058ec21cecf4f3199a2a03
Antariex <arielantequiera@gmail.com>
2024-03-16 00:39:18 -0300

    add missed printing datas exercises

---
+++ src/MagicLetter.java
@@ -0,0 +1,27 @@
+/**
+ Printing a magic letter
+
+ Write a program that prints the following magic letter using * and whitespaces.
+
+  Sample Output
+
+ ********
+       **
+       **
+       **
+ **    **
+ **    **
+ *******
+*/
+
+public class MagicLetter {
+    public static void main(String[] args) {
+        System.out.println("********");
+        System.out.println("      **");
+        System.out.println("      **");
+        System.out.println("      **");
+        System.out.println("**    **");
+        System.out.println("**    **");
+        System.out.println(" *******");
+    }
+}
