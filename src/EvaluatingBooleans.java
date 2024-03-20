eb486565462994183e8ea5761d7dd81e91a7724f
Antariex <arielantequiera@gmail.com>
2024-03-19 23:18:31 -0300

    Add booleans exercises

---
+++ src/EvaluatingBooleans.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class EvaluatingBooleans {
+    public static void main(String[] args) {
+        // Initialize Scanner object to read the input
+        Scanner in = new Scanner(System.in);
+
+        // Read two boolean inputs
+        boolean bool1 = in.nextBoolean();
+        boolean bool2 = in.nextBoolean();
+
+        // Logic to find the AND operation of 2 booleans
+        if (bool1 && bool2) {
+            System.out.println("true");
+        }
+        else System.out.println("false");
+
+        // Close the scanner
+        in.close();
+    }
+}
