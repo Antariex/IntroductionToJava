122d6f901054407cc5449f45f38c243a4f2361dd
Antariex <arielantequiera@gmail.com>
2024-03-20 21:02:20 -0300

    Add comparing values exercises

---
+++ src/CheckTheValue.java
@@ -0,0 +1,26 @@
+/**
+ Check the value
+
+ Write a program that reads a value and checks if it is less than 10.
+ It should print true if it is less than 10 or false otherwise.
+
+ Sample Input 1:
+ 5
+
+ Sample Output 1:
+ true
+ */
+
+import java.util.Scanner;
+
+public class CheckTheValue {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+
+        if (input < 10) {
+            System.out.println("true");
+        }
+        else System.out.println("false");
+    }
+}
