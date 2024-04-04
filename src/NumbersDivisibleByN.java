16c2d4f3e5a1bfa8dbf4b751cfb217db797fceef
Antariex <arielantequiera@gmail.com>
2024-04-03 22:07:10 -0300

    Add for loops exercises

---
+++ src/NumbersDivisibleByN.java
@@ -0,0 +1,28 @@
+/**
+ The count of numbers divisible by N
+
+ Write a program that reads three positive integers a, b, n and outputs the count of numbers divisible by n in the range
+ from a to b (a < b) inclusively.
+
+ Sample Input 1:
+ 10 20 10
+
+ Sample Output 1:
+ 2
+
+ Sample Input 2:
+ 15 25 5
+
+ Sample Output 2:
+ 3
+ */
+
+import java.util.Scanner;
+
+public class NumbersDivisibleByN {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
